4e14786456c25e46d7551dfa648365ef54dcf9e5
Yoshi Shih <ysshih@gmail.com>
2024-03-22 15:24:39 +0800

    receiver

--- library/src/main/java/com/inuker/bluetooth/library/utils/BluetoothUtils.java
+++ library/src/main/java/com/inuker/bluetooth/library/utils/BluetoothUtils.java
@@ -51,16 +51,21 @@ public class BluetoothUtils {
     }
 
     private static void registerGlobalReceiver(BroadcastReceiver receiver, IntentFilter filter) {
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    getContext().registerReceiver(receiver, filter, RECEIVER_EXPORTED);
-                } else getContext().registerReceiver(receiver, filter);
-            } else getContext().registerReceiver(receiver, filter);
-        } catch (Exception e) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
-                getContext().registerReceiver(receiver, filter, RECEIVER_EXPORTED);
-            else getContext().registerReceiver(receiver, filter);
+//        try {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                    getContext().registerReceiver(receiver, filter, RECEIVER_EXPORTED);
+//                } else getContext().registerReceiver(receiver, filter);
+//            } else getContext().registerReceiver(receiver, filter);
+//        } catch (Exception e) {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+//                getContext().registerReceiver(receiver, filter, RECEIVER_EXPORTED);
+//            else getContext().registerReceiver(receiver, filter);
+//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getContext().registerReceiver(receiver, filter, RECEIVER_EXPORTED)
+        }else {
+            getContext().registerReceiver(receiver, filter)
         }
     }
 
